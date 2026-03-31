82cc29ce073f3d6d51f55f61a7fc8207590015c9
Léo Pradel <pradel.leo@gmail.com>
2026-03-31 20:17:31 +0200

    Update index.ts

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,1 +1,16 @@
-export * as StacksApi from "./datasources/api/index.js";
+import { NodeHttpClient } from "@effect/platform-node";
+import { Effect } from "effect";
+
+import { StacksApiServiceLive } from "./datasources/api/index.js";
+import { StacksApiService } from "./datasources/api/StacksApiService.ts";
+
+const program = Effect.flatMap(StacksApiService, (api) => api.fetchBlock(100n)).pipe(
+  Effect.provide(
+    StacksApiServiceLive({
+      baseUrl: "https://api.hiro.so",
+    }),
+  ),
+  Effect.provide(NodeHttpClient.layer),
+);
+
+await Effect.runPromiseExit(program);
