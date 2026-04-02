9bf446685ea00ccad5a5463fcc533680f51fb93f
Léo Pradel <pradel.leo@gmail.com>
2026-04-02 15:58:24 +0200

    Update encode.ts

--- packages/utils/src/sync-store/encode.ts
+++ packages/utils/src/sync-store/encode.ts
@@ -1,4 +1,4 @@
-import type { BlockApiResponse } from "../datasources/api/index.ts";
+import type { BlockApiResponse, TransactionApiResponse } from "../datasources/api/index.ts";
 import type * as ponderSyncSchema from "./schema.js";
 
 export const encodeBlock = ({
@@ -13,4 +13,24 @@ export const encodeBlock = ({
   hash: block.hash,
   blockTime: BigInt(block.burn_block_time),
   tenureHeight: BigInt(block.burn_block_height),
+});
+
+export const encodeTransaction = ({
+  transaction,
+  chainId,
+}: {
+  transaction: TransactionApiResponse;
+  chainId: number;
+}): typeof ponderSyncSchema.transactionsTable.$inferInsert => ({
+  txId: transaction.tx_id,
+  chainId: BigInt(chainId),
+  blockHeight: BigInt(transaction.block_height),
+  blockHash: transaction.block_hash,
+  txIndex: transaction.tx_index,
+  txType: transaction.tx_type,
+  senderAddress: transaction.sender_address,
+  feeRate: BigInt(transaction.fee_rate),
+  nonce: BigInt(transaction.nonce),
+  txStatus: transaction.tx_status,
+  canonical: transaction.canonical,
 });
