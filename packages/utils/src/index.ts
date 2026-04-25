e44d59a12a1150fc29be730a5e970f6ce73484b7
Léo Pradel <pradel.leo@gmail.com>
2026-04-25 12:52:06 +0200

    Update index.ts

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -14,4 +14,11 @@ const logger = createLogger({
   level: 5,
 });
 const runtime = createHistoricalRuntime({ logger, db });
-await runtime.run([{ contractId: "SPGDS0Y17973EN5TCHNHGJJ9B31XWQ5YX8A36C9B.usdcx-poolv1" }]);
+
+try {
+  await runtime.run([{ contractId: "SPGDS0Y17973EN5TCHNHGJJ9B31XWQ5YX8A36C9B.usdcx-poolv1" }]);
+} catch (err) {
+  logger.error({ msg: "Error running historical sync", error: err });
+  // oxlint-disable-next-line no-undef
+  process.exit(1);
+}
