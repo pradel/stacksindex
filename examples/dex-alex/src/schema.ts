0903f5155971e8a7e1a8110acaa54f7a2d80db42
Léo Pradel <pradel.leo@gmail.com>
2026-04-29 19:26:09 +0200

    Update schema.ts

--- examples/dex-alex/src/schema.ts
+++ examples/dex-alex/src/schema.ts
@@ -15,6 +15,21 @@ export const tokenTable = pgTable(
   ],
 );
 
+export const poolTable = pgTable(
+  "pool",
+  (table) => ({
+    address: table.text("address").notNull(),
+    chainId: table.bigint({ mode: "bigint" }).notNull(),
+    baseToken: table.text("base_token").notNull(),
+    quoteToken: table.text("quote_token").notNull(),
+  }),
+  (table) => [
+    primaryKey({
+      columns: [table.address, table.chainId],
+    }),
+  ],
+);
+
 export const swapTable = pgTable(
   "swap",
   (table) => ({
