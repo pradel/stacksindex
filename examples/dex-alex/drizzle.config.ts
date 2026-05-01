39db794b7cf77912c6617fe13372b936497cf807
Léo Pradel <pradel.leo@gmail.com>
2026-05-01 19:56:23 +0200

    studio

--- examples/dex-alex/drizzle.config.ts
+++ examples/dex-alex/drizzle.config.ts
@@ -4,4 +4,8 @@ export default defineConfig({
   out: "./drizzle",
   schema: "./src/schema.ts",
   dialect: "postgresql",
+  driver: "pglite",
+  dbCredentials: {
+    url: "./data/app.db",
+  },
 });
