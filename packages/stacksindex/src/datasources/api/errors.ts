6337f05a4c241b5a138045eab1af84b4825544a0
Léo Pradel <pradel.leo@gmail.com>
2026-08-26 11:34:00 +0200

    chore: better-result v3

--- packages/stacksindex/src/datasources/api/errors.ts
+++ packages/stacksindex/src/datasources/api/errors.ts
@@ -4,24 +4,24 @@ export class StacksApiUnexpectedError extends TaggedError("StacksApiUnexpectedEr
   message: string;
   cause: unknown;
   path: string;
-}>() {}
+}> {}
 
 export class StacksApiResponseError extends TaggedError("StacksApiResponseError")<{
   status: number;
   path: string;
   statusText: string;
   errorData: unknown;
-}>() {}
+}> {}
 
 export class StacksApiRateLimitError extends TaggedError("StacksApiRateLimitError")<{
   path: string;
   retryAfter: number;
-}>() {}
+}> {}
 
 export class StacksApiParseError extends TaggedError("StacksApiParseError")<{
   message: string;
   cause: unknown;
-}>() {}
+}> {}
 
 export type StacksApiError =
   | StacksApiUnexpectedError
